c7f9ac3f6ca504b117f82524bac52e39ad078d3a
Jeff Oliver <jeffoliver3384@gmail.com>
2020-10-14 14:32:35 -0400

    add congrats component and test utils

---
+++ jotto/test/testUtils.js
@@ -0,0 +1,11 @@
+import { ShallowWrapper } from "enzyme";
+
+/**
+ * Returns node(s) with the given data-test attribute.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
+ * @param {string} value - Value of data-test attribute for search.
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper, value) => {
+  return wrapper.find(`[data-test='${value}']`);
+};
