595250c514a8122ddc8468d4b78af04656a1742a
Jeff Oliver <jeffoliver3384@gmail.com>
2020-10-26 16:40:29 -0400

    turned app functional component to a class component and ren

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
+import Congrats from "./Congrats";
+import GuessedWords from "./GuessedWords";
 import "./App.css";
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Jotto</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords
+          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
