a2a978bebfe1d942981173e0886554ca5fde77b3
Jeff Oliver <jeffoliver3384@gmail.com>
2021-01-11 12:09:15 -0500

    Make tests and code for guessWord arg

--- jotto/src/Input.js
+++ jotto/src/Input.js
@@ -4,6 +4,32 @@ import { connect } from "react-redux";
 import { guessWord } from "./actions";
 
 export class UnconnectedInput extends Component {
+  /**
+   * @method constructor
+   * @param {object} props - Component props.
+   * @returns {undefined}
+   */
+  constructor(props) {
+    super(props);
+
+    // initialize state
+    this.state = {
+      currentGuess: null,
+    };
+
+    // bind this for submitGuessedWord
+    this.submitGuessedWord = this.submitGuessedWord.bind(this);
+  }
+
+  submitGuessedWord(e) {
+    e.preventDefault();
+    const guessedWord = this.state.currentGuess;
+
+    if (guessedWord && guessedWord.length > 0) {
+      this.props.guessWord(guessedWord);
+    }
+  }
+
   render() {
     const contents = this.props.success ? null : (
       <form className='form-inline'>
@@ -11,12 +37,14 @@ export class UnconnectedInput extends Component {
           data-test='input-box'
           className='mb-2 mx-sm-3'
           type='text'
+          value={this.state.currentGuess}
+          onChange={(e) => this.setState({ currentGuess: e.target.value })}
           placeholder='enter guess'
         />
         <button
           data-test='submit-button'
           className='btn btn-primary mb-2'
-          onClick={() => this.props.guessWord("train")}
+          onClick={this.submitGuessedWord}
           type='submit'
         >
           Submit
